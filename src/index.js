8a3568681250410a70bf6cbc6e2dd845631f5f90
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-05-18 01:33:31 -0300

    Resolvido e adicionado exercicio 06

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const calcularInss = require("./calculo_inss");
 const readline = require("readline");
 const calcularSalarioLiquido = require("./calculo_salario_liquido");
 const formatarCpf = require("./formatarCpf");
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
+const doc = new PDFDocument();
+
 const input = readline.createInterface(process.stdin, process.stdout);
 const folhaPagamento = {
   nome: "",
@@ -46,7 +50,59 @@ input.question("Nome: ", (nome) => {
         console.log(
           `Salário Líquido: R$ ${folhaPagamento.salarioLiquido.toFixed(2)}`
         );
-        input.close();
+
+        // ... (código anterior)
+
+        input.question(
+          "Deseja gerar um PDF com a folha de pagamento? (s/n) ? ",
+          (resposta) => {
+            if (resposta.toLowerCase() === "s") {
+              if (!fs.existsSync("src/folhas_pagamentos")) {
+                fs.mkdirSync("src/folhas_pagamentos");
+
+                console.log("Gerando PDF...");
+                const nomeArquivo = `src/folhas_pagamentos/folha_pagamento_${folhaPagamento.nome}.pdf`;
+
+                doc.pipe(fs.createWriteStream(nomeArquivo));
+                doc.fontSize(16);
+                doc.text("--- Folha de Pagamento ---");
+                doc.text(`Data de Geração: ${new Date().toLocaleDateString()}`);
+                doc.text(`Nome: ${folhaPagamento.nome}`);
+                doc.text(`CPF: ${folhaPagamento.cpf}`);
+                doc.text(`Mês do Pagamento: ${folhaPagamento.mesPagamento}`);
+                doc.text("--- ---");
+                doc.text(
+                  `Salário Bruto: R$ ${folhaPagamento.salarioBruto.toFixed(2)}`
+                );
+                doc.text("--- Descontos ---");
+                doc.text(`INSS: R$ ${folhaPagamento.inss.toFixed(2)}`);
+                doc.text(
+                  `Imposto de Renda: R$ ${folhaPagamento.impostoRenda.toFixed(
+                    2
+                  )}`
+                );
+                doc.text(
+                  `Outros Descontos: R$ ${folhaPagamento.outrosDescontos.toFixed(
+                    2
+                  )}`
+                );
+                doc.text("--- ---");
+                doc.text(
+                  `Salário Líquido: R$ ${folhaPagamento.salarioLiquido.toFixed(
+                    2
+                  )}`
+                );
+                doc.end();
+                console.log(`Folha de pagamento salva em ${nomeArquivo}!`);
+              }
+            } else {
+              console.log(
+                "Obrigado por utilizar o sistema de folha de pagamento!"
+              );
+            }
+            input.close();
+          }
+        );
       });
     });
   });
