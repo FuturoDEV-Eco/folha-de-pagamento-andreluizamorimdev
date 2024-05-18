24a9a27fae1c9ffdb29d9d1c33accc21d49518f3
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-05-18 00:23:30 -0300

    Resolvido e adicionado exercicio04

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const calcularImpostoRenda = require("./calculo_imposto_renda");
 const calcularInss = require("./calculo_inss");
 const readline = require("readline");
+const calcularSalarioLiquido = require("./calculo_salario_liquido");
 const input = readline.createInterface(process.stdin, process.stdout);
 const folhaPagamento = {
   nome: "",
   salarioBruto: 0,
   inss: 0,
   impostoRenda: 0,
+  outrosDescontos: 0,
   salarioLiquido: 0
 };
 
@@ -19,6 +21,16 @@ input.question("Qual o seu salario? ", (salario) => {
   );
   console.log(
     "Imposto de Renda a ser pago: R$ " + folhaPagamento.impostoRenda.toFixed(2)
+  );
+
+  folhaPagamento.salarioLiquido = calcularSalarioLiquido(
+    folhaPagamento.salarioBruto,
+    folhaPagamento.inss,
+    folhaPagamento.impostoRenda,
+    folhaPagamento.outrosDescontos
+  );
+  console.log(
+    "Salario Liquido: R$ " + folhaPagamento.salarioLiquido.toFixed(2)
   );
   input.close();
 });
