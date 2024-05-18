fae7caf297b2239d3fef96bba568e06aad0d941c
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-05-17 23:31:09 -0300

    adicionando readline para pegar o salarioBruto digitado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 const calcularInss = require("./calculo_inss");
+const readline = require("readline");
+const input = readline.createInterface(process.stdin, process.stdout);
+const folhaPagamento = {
+  nome: "",
+  salarioBruto: 0,
+  inss: 0,
+  impostoRenda: 0,
+  salarioLiquido: 0
+};
 
-console.log(calcularInss(5600));
+input.question("Qual o seu salario? ", (salario) => {
+  folhaPagamento.salarioBruto = Number(salario);
+  folhaPagamento.inss = calcularInss(folhaPagamento.salarioBruto);
+  console.log("INSS: " + folhaPagamento.inss.toFixed(2));
+  input.close();
+});
